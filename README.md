2815165f54e1a326dd5c6c6436743ba7b2824a16
Tim Caswell <tim@creationix.com>
2014-07-19 15:27:02 -0500

    Initial implementation

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ coreson
 =======
 
 Allow js-git to clone from the browser by turning CORS response headers in a chrome extension
+
+Clone this repo to your desktop, add as extension to chrome.
+
+A new icon with default icon will appear.  When it's clicked it will enable CORS to all domains for that tab.
+
+For example, go to http://clone-test.creationix.com/ after installing the plugin.
+
+Once it's activated, you can clone over https using direct XHR.
---
+++ background.js
@@ -0,0 +1,65 @@
+"use strict";
+var webRequest = window.chrome.webRequest;
+var browserAction = window.chrome.browserAction;
+
+var filter = {
+  urls: ["<all_urls>"],
+  types: ["xmlhttprequest", "other"]
+};
+var agent = "git/1.9.3";
+
+var extraHeaders = [
+  {
+    name: "Access-Control-Allow-Origin",
+    value: "*",
+  },
+  {
+    name: "Access-Control-Allow-Methods",
+    value: "POST, GET",
+  },
+  {
+    name: "Access-Control-Allow-Headers",
+    value: "Content-Type",
+  }
+];
+
+browserAction.onClicked.addListener(function (tab) {
+  setTab(tab.id);
+});
+
+function setTab(tabId) {
+  browserAction.setBadgeText({
+    text: "ON",
+    tabId: tabId
+  });
+  filter.tabId = tabId;
+
+  // Add CORS headers to responses so we can talk git cross-domain.
+  // webRequest.onHeadersReceived.removeListener()
+  webRequest.onHeadersReceived.addListener(onHeadersReceived, filter, ["blocking", "responseHeaders"]);
+  // // Change the agent to make bitbucket happy
+  // webRequest.onBeforeSendHeaders.addListener(onBeforeSendHeaders, filter, ["blocking", "requestHeaders"]);
+}
+
+function onHeadersReceived(details) {
+  console.log("onHeadersReceived");
+  var headers = details.requestHeaders;
+  for (var i = 0; i < headers.length; i++) {
+
+  }
+  return {
+    responseHeaders: details.responseHeaders.concat(extraHeaders)
+  };
+}
+
+// function onBeforeSendHeaders(details) {
+//   var headers = details.requestHeaders;
+//   for (var i = 0; i < headers.length; i++) {
+//     if (headers[i].name.toLowerCase() === 'user-agent') {
+//       console.log("Changing User-Agent to " + agent);
+//       headers[i].value = agent;
+//       break;
+//     }
+//   }
+//   return { requestHeaders: headers };
+// }
